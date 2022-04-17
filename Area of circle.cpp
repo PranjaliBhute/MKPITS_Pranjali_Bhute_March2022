4b67455a69b673a03c08e55556f41cef0ecb00d7
PranjaliBhute <103639382+PranjaliBhute@users.noreply.github.com>
2022-04-17 22:54:39 +0530

    Area of circle formula.

---
+++ Area of circle.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int radi,res;
+	float pi=3.14;
+	printf("Enter radius of circle");
+	scanf("%d",&radi);
+	res=3.14*radi*radi;
+	printf("%d",res);
+}
