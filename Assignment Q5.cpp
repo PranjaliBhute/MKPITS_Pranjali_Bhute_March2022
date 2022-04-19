65523f8d8e4ca982235705d43b5f33e59e1e29fc
PranjaliBhute <103639382+PranjaliBhute@users.noreply.github.com>
2022-04-19 22:54:52 +0530

    The result of dividing two numbers.(Assign Q5)

---
+++ Assignment Q5.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int a,b,res;
+	printf("Enter two number");
+	scanf("%d%d",&a,&b);
+	res=a/b;
+	printf("Result of dividing two numbers is %d",res);
+}
