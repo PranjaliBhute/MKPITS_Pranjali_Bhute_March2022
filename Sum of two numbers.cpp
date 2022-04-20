eb6862a8c0fe118b915eaa6b7b710c39a1d3fadb
PranjaliBhute <103639382+PranjaliBhute@users.noreply.github.com>
2022-04-20 22:03:17 +0530

    Assign Q3-Sum of two numbers.

---
+++ Sum of two numbers.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int num1,num2,sum;
+	printf("Enter two numbers");
+	scanf("%d%d",&num1,&num2);
+	sum=num1+num2;
+	printf("Sum of two numbers %d",sum);
+}
