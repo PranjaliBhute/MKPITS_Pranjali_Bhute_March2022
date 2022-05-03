af9c07308578108e3c35ee09112f4f53da8857c7
PranjaliBhute <103639382+PranjaliBhute@users.noreply.github.com>
2022-05-03 15:38:39 +0530

    sum of two no.(Assignment Q10)

---
+++ Assignment Q10.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int a,b,sum;
+	printf("Enter the two integers from user ");
+	scanf("%d%d",&a,&b);
+	sum=a+b;
+	printf("Sum of two numbers is %d",sum);
+	
+}
