d19de2190ca5144677acbad2a6c4891e6d040387
PranjaliBhute <103639382+PranjaliBhute@users.noreply.github.com>
2022-05-05 12:52:16 +0530

    call by value pgm

---
+++ Call by value pgm.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void fun(int x, int y);
+int main()
+{
+	int a=2,b=3;
+	printf("a=%d b=%d",a,b);
+	fun(a,b);
+	printf("\na=%d b=%d",a,b);
+}
+ void fun(int x, int y)
+{
+  x=12;
+  y=13;	
+}
