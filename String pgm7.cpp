1404a0f3a93ff36f838e925695fde8be16f69d01
PranjaliBhute <103639382+PranjaliBhute@users.noreply.github.com>
2022-05-08 19:23:46 +0530

    String pgm7 strlen().

---
+++ String pgm7.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+main()
+{
+	char name[20]={"Saanvi"};
+	int len;
+	//printf("Enter any name ");
+	//scanf("%s",name);
+	len=strlen(name);
+	printf("%d",len);
+}
