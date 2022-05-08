f04c9084f09b05550cd28d93f9ab5012d12d0f58
PranjaliBhute <103639382+PranjaliBhute@users.noreply.github.com>
2022-05-08 19:29:45 +0530

    String pgm8 strcat().

---
+++ String pgm8.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+	char name1[20]={"Vaibhav"};
+	char name2[20]={"Pranjali"};
+	strcat(name2,name1);
+	printf("%s",name2);
+
+}
