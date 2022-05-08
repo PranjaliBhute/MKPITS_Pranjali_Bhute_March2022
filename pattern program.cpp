51bfb8398007df0d3ad1743d1edd00560e74a46c
PranjaliBhute <103639382+PranjaliBhute@users.noreply.github.com>
2022-05-08 14:57:25 +0530

    pattern program

---
+++ pattern program.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int r,c,n='a';
+	for(r=1;r<=4;r++)
+	{
+		for(c=1;c<=5;c++)
+	 { 
+	     if(r%2!=0)
+	     {
+	     	printf("%d",c);
+		 }
+		 if(r%2==0)
+		 {
+		 	printf("%c",n);
+		 }
+	 }
+	 printf("\n");
+    }
+}
