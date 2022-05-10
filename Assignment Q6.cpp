de7eed5863e154ceb7e86535c5d581e54f3e4570
PranjaliBhute <103639382+PranjaliBhute@users.noreply.github.com>
2022-05-10 13:34:27 +0530

    Reverse character prg

---
+++ Assignment Q6.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	char c1, c2, c3;
+	printf("Test character:'X''M''L' ");
+	c1='X';
+	c2='M';
+	c3='L';
+	printf("\nThe reverse of XML is %c%c%c ",c3,c2,c1);
+}
