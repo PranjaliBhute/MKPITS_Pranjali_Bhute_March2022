a6c407475715f9a7f9f465338519120fb0453aa3
PranjaliBhute <103639382+PranjaliBhute@users.noreply.github.com>
2022-05-12 21:41:48 +0530

    The correct password is 1234.

---
+++ Assignment Q32.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int pass;
+	printf("Enter a password ");
+	scanf("%d",&pass);
+	if(pass==1234)
+	{
+		printf("Correct Password");
+	}
+	else
+	{
+		printf("Incorrect Password");
+	}
+}
