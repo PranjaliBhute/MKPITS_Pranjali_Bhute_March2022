fb3f1fb302d6aa97051910add6acab6a382594eb
PranjaliBhute <103639382+PranjaliBhute@users.noreply.github.com>
2022-05-13 17:57:25 +0530

    Alphabet pattern pgm5

---
+++ Alphabet pattern pgm5.cpp
@@ -0,0 +1,18 @@
+/*EDCBA
+  EDCBA
+  EDCBA
+  EDCBA*/
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int r,c;
+	for(r=1;r<=4;r++)
+	{
+		for(c='E';c>='A';c--)
+		{
+			printf("%c",c);
+		}
+		printf("\n");
+	}
+}
