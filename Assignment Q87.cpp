67d27f6d755379133e061fbf17aa454bfb3f2752
PranjaliBhute <103639382+PranjaliBhute@users.noreply.github.com>
2022-05-14 22:38:16 +0530

    Last digit are same or not.

---
+++ Assignment Q87.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int num1,num2,l1,l2;
+	printf("Enter 1st number ");
+	scanf("%d",&num1);
+	printf("Enter 2nd number ");
+	scanf("%d",&num2);
+	l1=num1%10;
+	l2=num2%10;
+	if(l1==l2)
+	{
+	    printf("Last digit are same ");
+    }
+	else
+	{
+		printf("Last digit are not same ");
+	}
+	
+}	
