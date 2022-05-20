b4c908bd1b887fd7189465fedb72cc837baa1ed1
PranjaliBhute <103639382+PranjaliBhute@users.noreply.github.com>
2022-05-20 23:23:43 +0530

    Assignment Q29-read 5 numbers and sum of all even values between them.

---
+++ Assignment Q29.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int i,num[5],sum=0;
+	printf("Input 1st number ");
+	scanf("%d",&num[0]);
+	printf("Input 2nd number ");
+	scanf("%d",&num[1]);
+    printf("Input 3rd number ");
+	scanf("%d",&num[2]);
+	printf("Input 4th number ");
+	scanf("%d",&num[3]);
+    printf("Input 5th number ");
+	scanf("%d",&num[4]);
+	for(i=0;i<5;i++)
+	{
+		if((num[i]%2) == 0)
+		{
+			sum += num[i];
+		}
+	}
+	printf("Sum of all even values %d",sum);
+	printf("\n");
+	return 0;
+}
