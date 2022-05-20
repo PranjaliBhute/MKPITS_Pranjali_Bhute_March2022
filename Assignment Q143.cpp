c05c2f228b183aa37ef93329499c0c44afcdf037
PranjaliBhute <103639382+PranjaliBhute@users.noreply.github.com>
2022-05-20 21:56:27 +0530

    Assignment Q143-four no. as input to calculate and print the average.

---
+++ Assignment Q143.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int a,b,c,d;
+	float e;
+	printf("Enter the value of four variable\n");
+	scanf("%d%d%d%d",&a,&b,&c,&d);
+	e=(a+b+c+d)/4;
+	printf("%f",e);
+	
+}
