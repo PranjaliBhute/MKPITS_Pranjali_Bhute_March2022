6bd62865545baabd583c3c4aaee454b04c423be6
PranjaliBhute <103639382+PranjaliBhute@users.noreply.github.com>
2022-05-20 22:35:26 +0530

    Assignment Q24-check two integers multiplied or not.

---
+++ Assignment Q24.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int n1,n2;
+	printf("Enter two integer number ");
+	scanf("%d%d",&n1,&n2);
+	if(n1>n2)
+	{
+		if(n1%n2==0)
+		{
+			printf("%d is multiple of %d ",n1,n2);
+		}
+		else
+		{
+			printf("%d is not multiple of %d ",n1,n2);
+		}
+	}
+	else
+	if(n2%n1==0)
+	{
+		printf("%d is multiple of %d ",n2,n1);
+	}
+	else
+	{
+		printf("%d is not multiple of %d ",n2,n1);
+	}
+	
+}
