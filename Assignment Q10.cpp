cef8f21728fc557bf2cfe5112b35b6e916cb8344
PranjaliBhute <103639382+PranjaliBhute@users.noreply.github.com>
2022-05-24 15:27:20 +0530

    Assignment Q10-Calculate the sum of the two integers.

--- Assignment Q10.cpp
+++ Assignment Q10.cpp
@@ -1,11 +1,13 @@
 #include<stdio.h>
 #include<conio.h>
-main()
-{
-	int a,b,sum;
-	printf("Enter the two integers from user ");
-	scanf("%d%d",&a,&b);
-	sum=a+b;
-	printf("Sum of two numbers is %d",sum);
-	
+int main() 
+   {
+	int x, y, sum;
+    printf("Input the first integer: "); 
+    scanf("%d", &x);
+    printf("Input the second integer: ");
+    scanf("%d", &y);
+    sum = x + y;
+    printf("Sum of the two integers = %d\n", sum);
+    
 }
