c95ad669eb6b346ebf028b7d906308e1cbaabe9d
PranjaliBhute <103639382+PranjaliBhute@users.noreply.github.com>
2022-05-24 15:01:16 +0530

    Assignment Q2-print name ,death of birth and mobile no.

---
+++ Assignment Q2.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h> 
+ int main()  
+  {
+     printf("Name   : Alexandra Abramov\n"); 
+     printf("DOB    : July 14, 1975\n"); 
+     printf("Mobile : 99-9999999999\n"); 
+     
+  }
