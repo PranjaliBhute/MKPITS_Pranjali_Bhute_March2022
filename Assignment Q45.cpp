9b2039f1b249a47ac35de7fb8f4f6680ac929fcf
PranjaliBhute <103639382+PranjaliBhute@users.noreply.github.com>
2022-05-24 15:58:24 +0530

    Assignment Q45-read and print the element of an array.

---
+++ Assignment Q45.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main() 
+{
+	int n[7], i, x;
+	printf("Input the first number of the array:\n");
+
+	scanf("%d", &x);
+	for(i=0;i<7;i++) 
+	{
+		n[i] = x;
+		x = 3*x;
+	}
+
+	for(i=0;i<7;i++)
+	{
+		printf("n[%d] = %d\n", i, n[i]);
+	}
+
+}
