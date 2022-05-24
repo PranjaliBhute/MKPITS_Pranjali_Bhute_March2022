9d9380dd4f3591e9990b20d9a3f02fd204c4945e
PranjaliBhute <103639382+PranjaliBhute@users.noreply.github.com>
2022-05-24 21:52:32 +0530

    Assignment Q52-to shift given data by two bits to the left.

---
+++ Assignment Q52.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a, b;
+printf("Enter integer no. ");
+scanf("%d",&a);
+printf("Integer value = %d ",a);
+a<<=2;
+b=a;
+printf("\nThe left shifted data is = %d ",b);
+}
