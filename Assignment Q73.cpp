62bc2006408a40681450bd5156392ea491d707cf
PranjaliBhute <103639382+PranjaliBhute@users.noreply.github.com>
2022-06-22 17:29:14 +0530

    Assignment Q73-Multiplication of three no. which will be entered by the user.

---
+++ Assignment Q73.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+   int a,b,c,mul;
+   printf("Enter First No. ");
+   scanf("%d",&a);
+   printf("Enter Second No. ");
+   scanf("%d",&b);
+   printf("Enter Third No. ");
+   scanf("%d",&c);
+   mul=a*b*c;
+   printf("Multiplication of entered no. is = %d",mul);
+}
