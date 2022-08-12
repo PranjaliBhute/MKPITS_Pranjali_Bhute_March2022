8e4405d3263d590b3399187d898ee8adadb42da2
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-12 19:19:00 +0530

    Program of Employee details using if_else.

---
+++ Csharp/Assignment2/Program5.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeDetailsUsingIfelse
+{
+    internal class EmployeeDetailsProgram
+    {
+        static void Main(string[] args)
+        {
+            string empname, designation;
+            int bonus = 0, empno;
+            Console.WriteLine("Enter Employee number : ");
+            empno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Employee name : ");
+            empname = Console.ReadLine();
+            Console.WriteLine("Enter designation : ");
+            designation = Console.ReadLine();
+            if (designation == "manager")
+            {
+                bonus = 10000;
+            }
+            else if (designation == "clerk")
+            {
+                bonus = 5000;
+            }
+            else if (designation == "peon")
+            {
+                bonus = 2000;
+            }
+            else
+            {
+                Console.WriteLine("Invalid designation");
+            }
+            Console.WriteLine("Bonus = " + bonus);
+            Console.ReadLine();
+        }
+    }
+}
