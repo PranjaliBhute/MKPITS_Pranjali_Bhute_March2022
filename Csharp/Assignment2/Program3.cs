2e729fd236107a6f05105797f17ae488d2ac57d2
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-12 19:13:18 +0530

    Character vowel or not using switch.

---
+++ Csharp/Assignment2/Program3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharVowelOrNotUsingSwitch
+{
+    internal class VowelUsingSwitch
+    {
+        static void Main(string[] args)
+        {
+            char choice;
+            Console.WriteLine("Enter character : ");
+            choice = Convert.ToChar(Console.ReadLine());
+            switch (choice)
+            {
+                case 'a':
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                    Console.WriteLine("Entered char is vowel.");
+                    break;
+                default:
+                    Console.WriteLine("Entered char is not vowel.");
+                    break;
+             
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
