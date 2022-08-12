d081f54886529bd7330f7a1637fb20e8ff552dc7
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-12 18:28:15 +0530

    Program of Employee Details.

---
+++ Oops/Assignment2/Program2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramOfEmployeeDetails
+{
+    class Employee
+    {
+        int empno;
+        string empname;
+        string designation;
+        int salary;
+
+        public Employee()
+        {
+            empno = 1;
+            empname = "Pranjali";
+            designation = "Manager";
+            salary = 30000;
+        }
+        public void display()
+         {
+                Console.WriteLine("Employee No. = " + empno);
+                Console.WriteLine("Employee Name = " + empname);
+                Console.WriteLine("Designation = " + designation);
+                Console.WriteLine("Employee salary = " + salary);
+         }
+     }
+     internal class Program
+      {
+         static void Main(string[] args)
+         {
+                Employee emp = new Employee();
+                emp.display();
+                Console.ReadLine();
+         }
+     }
+}
