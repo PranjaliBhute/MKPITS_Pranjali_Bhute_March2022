411d89a01da2b57e5a21715be27a35b8fa56dccf
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-12 18:25:36 +0530

    Program of Product Details.

---
+++ Oops/Assignment2/Program1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramOfProductDetails
+{
+    class Product
+    {
+        int productid;
+        string productname;
+        int price;
+        int quantity;
+
+        public Product()
+        {
+            productid = 1;
+            productname = "Pen";
+            price = 20;
+            quantity = 10;
+        }
+        public void display()
+        {
+            Console.WriteLine("Productid = " + productid);
+            Console.WriteLine("Productname = " + productname);
+            Console.WriteLine("Price = " + price);
+            Console.WriteLine("Quantity = " + quantity);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product P = new Product();
+            P.display();
+            Console.ReadLine();
+        }
+    }
+}
