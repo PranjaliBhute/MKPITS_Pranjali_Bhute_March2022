e1e93958609c09a5669a0b2f1b3389a88b95cbb2
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-12 17:27:49 +0530

    Program of Product Details.

---
+++ Oops/Assignment1/Program3.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramOfProductDetails
+{
+    class Product
+    {
+        int prodid;
+        string prodname;
+        float prodrate;
+        int quantity;
+
+
+        public void productdata()
+        {
+            Console.WriteLine("Enter product id = ");
+            prodid = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter product name = ");
+            prodname = Console.ReadLine();
+            Console.WriteLine("Enter product rate = ");
+            prodrate = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Enter quantity= ");
+            quantity = Convert.ToInt32(Console.ReadLine());
+        }
+        public void displayorder()
+        {
+            Console.WriteLine("prodid = " + prodid);
+            Console.WriteLine("prodname = " + prodname);
+            Console.WriteLine("prodrate = " + prodrate);
+            Console.WriteLine("quantity = " + quantity);
+
+        }
+
+    }
+        internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Product prod = new Product();
+            prod.productdata();
+            prod.displayorder();
+
+            Console.ReadLine();
+
+        }
+    }
+}
