97e2631b4b2c6cd3bd1c8b636d1231ab11b1037f
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-12 13:54:23 +0530

    Program of Area of Rectangle.

---
+++ Csharp/Assignment1/Program2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaOfRectangle
+{
+    internal class AreaOfRectangle
+    {
+        static void Main(string[] args)
+        {
+            int l, w;
+            Console.WriteLine("Enter length : ");
+           l= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter width : ");
+           w= Convert.ToInt32(Console.ReadLine());
+            int a;
+            a = l * w;
+            Console.WriteLine("Area of Rectangle = " + a);
+            Console.ReadLine();
+        }
+    }
+}
