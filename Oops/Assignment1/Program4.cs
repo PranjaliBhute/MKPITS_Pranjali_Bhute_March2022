d8e12446180afa4ac7e92c2b6f59b802bbec0bae
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-12 17:30:06 +0530

    Program of Car Details.

---
+++ Oops/Assignment1/Program4.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramOfCarDetails
+{
+    class Cardata
+    {
+        int carno;
+        string carmodel;
+        float carprice;
+        float caracc;
+
+
+        public void getcardata()
+        {
+            Console.WriteLine("Enter car number = ");
+            carno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter car name = ");
+            carmodel = Console.ReadLine();
+            Console.WriteLine("Eenter car price = ");
+            carprice = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Enter caracc = ");
+            caracc = Convert.ToSingle(Console.ReadLine());
+        }
+        public void displaycardata()
+        {
+            Console.WriteLine("carno = " + carno);
+            Console.WriteLine("carmodel = " + carmodel);
+            Console.WriteLine("carprice = " + carprice);
+            Console.WriteLine(" caracc = " + caracc);
+
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cardata car = new Cardata();
+            car.getcardata();
+            car.displaycardata();
+
+            Console.ReadLine();
+
+        }
+    }
+}
