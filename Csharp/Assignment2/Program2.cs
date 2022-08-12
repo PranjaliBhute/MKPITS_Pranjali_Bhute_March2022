a9f400c7372077508d8cea2e73aefe16873307c6
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-12 19:04:59 +0530

    Program of Character vowel or not.

---
+++ Csharp/Assignment2/Program2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharVowelOrNot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char choice;
+            Console.WriteLine("Enter character : ");
+            choice = Convert.ToChar(Console.ReadLine());
+            if (choice == 'a')
+                Console.WriteLine("Char is vowel.");
+            else if (choice == 'e')
+                Console.WriteLine("Char is vowel.");
+            else if (choice == 'i')
+                Console.WriteLine("Char is vowel.");
+            else if (choice == 'o')
+                Console.WriteLine("Char is vowel.");
+            else if (choice == 'u')
+                Console.WriteLine("Char is vowel.");
+            else
+                Console.WriteLine("Char is not vowel.");
+            Console.ReadLine();
+        }
+    }
+}
