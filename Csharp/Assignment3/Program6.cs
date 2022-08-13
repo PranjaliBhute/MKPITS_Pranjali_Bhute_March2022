603358d82eb8cc93a43cc24d8a2785e517e67f51
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 14:58:23 +0530

    Print table using do while loop.

---
+++ Csharp/Assignment3/Program6.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintTableUsingDoWhileLoop
+{
+    internal class PrintTable
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int res = 1;
+            Console.WriteLine("Enter number : ");
+            num = Convert.ToInt32(Console.ReadLine());
+            int i = 1;
+            do
+            {
+                res = num * i;
+                Console.WriteLine("{0} * {1} = {2}", num, i, res);
+                i++;
+
+            } while (i <= 10);
+            Console.ReadLine();
+        }
+    }
+}
