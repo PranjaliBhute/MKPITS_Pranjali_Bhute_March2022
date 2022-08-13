74c7b50be7030dff794957871a088ffd0790cdb0
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 20:00:43 +0530

    Employee details using method.

---
+++ Csharp/PracticeProgram/Program47.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeDetailsUsingMethod
+{
+    class Employee
+    {
+         int empno;
+         string empname;
+         float salary;
+
+        public  void getdata(int a, string b, float c)
+        {
+            empno = a;
+            empname = b;
+            salary = c;
+        }
+        public void displaydata()
+        {
+            Console.WriteLine("empno = " + empno);
+            Console.WriteLine("empname = " + empname);
+            Console.WriteLine("salary = " + salary);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            Console.WriteLine("Enter employee number : ");
+            int eno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter employee name : ");
+            string en = Console.ReadLine();
+            Console.WriteLine("Enter salary : ");
+            float sal = Convert.ToSingle(Console.ReadLine());
+
+            emp.getdata(eno,en,sal);
+            emp.displaydata();
+            Console.ReadLine();
+        }
+    }
+}
