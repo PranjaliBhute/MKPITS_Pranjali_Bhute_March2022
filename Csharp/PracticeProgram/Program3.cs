91e70d3a51ddf2b4dc1ec0df68e7354ea6f98af8
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 15:44:56 +0530

    Accept radius.

---
+++ Csharp/PracticeProgram/Program3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcceptRedius
+{
+    internal class DisplayAreaAndCirc
+    {
+        static void Main(string[] args)
+        {
+            int radius;
+            Console.WriteLine("Enter Radius : ");
+            radius = Convert.ToInt32(Console.ReadLine());
+            float area = 3.14f * radius * radius;
+            Console.WriteLine("Area = " + area);
+            float circ = 2 * 3.14f * radius;
+            Console.WriteLine("Circ = " + circ);
+            Console.ReadLine();
+
+        }
+    }
+}
