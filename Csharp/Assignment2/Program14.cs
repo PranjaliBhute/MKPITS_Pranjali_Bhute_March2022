7ae44478ceca4c838039f2824b70c7d0939497e4
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 12:37:50 +0530

    Program of prime number or not.

---
+++ Csharp/Assignment2/Program14.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramOfPrimeNumOrNot
+{
+    internal class PrimeNumber
+    {
+        static void Main(string[] args)
+        {
+            int num, i, flag = 0;
+            Console.WriteLine("Enter number : ");
+            num = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    flag = flag + 1;
+                }
+
+            }
+            if (flag == 2)
+            {
+                Console.WriteLine("Entered number is prime.");
+            }
+            else
+            {
+                Console.WriteLine("Entered number is not prime.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
