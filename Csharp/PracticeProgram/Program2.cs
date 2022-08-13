37dd759c0146b15e10cb3aa4e2f59bae1dcb5610
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 15:43:13 +0530

    Accept character from user.

---
+++ Csharp/PracticeProgram/Program2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcceptCharFromUser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ch;
+            Console.WriteLine("Enter Character : ");
+            ch = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Enter Character = " + ch);
+            Console.ReadLine();
+        }
+    }
+}
