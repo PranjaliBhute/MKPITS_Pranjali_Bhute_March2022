dc2e2e8c713b95db87b51a4be2d851ba7dcd64d5
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 23:55:52 +0530

    Example of protected access modifier.

---
+++ Csharp/PracticeProgram/Program70.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleOfProtectedAccessModifier
+{
+    public class Person
+    {
+        String name;
+        String address;
+        protected void getPersonData(String name, String address)
+        {
+            this.name = name;
+            this.address = address;
+        }
+        protected void displayPersonData()
+        {
+            Console.WriteLine("name : " + name);
+            Console.WriteLine("address : " + address);
+
+        }
+    }
+    public class Student : Person
+    {
+        int rno;
+        int marks;
+        public void getStudentData(int rno, int marks)
+        {
+            getPersonData("Saanvi", "Pratap nagar"); 
+            this.rno = rno;
+            this.marks = marks;
+        }
+        public void displayStudentData()
+        {
+            displayPersonData(); 
+            Console.WriteLine("rno : " + rno);
+            Console.WriteLine("marks : " + marks);
+
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student p1 = new Student();
+                                      
+            p1.getStudentData(111, 999);
+            p1.displayStudentData();
+            Console.ReadLine();
+        }
+    }
+}
