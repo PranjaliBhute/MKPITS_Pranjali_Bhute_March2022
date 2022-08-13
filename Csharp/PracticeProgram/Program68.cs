71076166732b02e7b36c08b66397ec299a2c79a1
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 23:51:20 +0530

    Example of inheritance specifier.

---
+++ Csharp/PracticeProgram/Program68.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleOfInheritanceSpecifier
+{
+    class Person
+    {
+        public string name;
+    }
+    class Student : Person 
+    {
+        public int rollno;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student S = new Student();
+            S.rollno = 11;
+            S.name = "Saanvi";
+            Console.WriteLine("rollno = " + S.rollno);
+            Console.WriteLine("name = " + S.name);
+            Console.ReadLine();
+        }
+    }
+}
