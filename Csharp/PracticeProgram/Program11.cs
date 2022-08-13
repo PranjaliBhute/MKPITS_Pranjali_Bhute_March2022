bdc92e8ebaf5757f49adeee6bd9e7f29c280c650
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 18:06:31 +0530

    Display result using switch.

---
+++ Csharp/PracticeProgram/Program11.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisplayResultUsingSwitch
+{
+    internal class DisplayResult
+    {
+        static void Main(string[] args)
+        {
+            int s1, s2;
+            char op;
+            int res = 0;
+            Console.WriteLine("Enter first number : ");
+            s1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number : ");
+            s2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter operator like + - * /");
+            op = Convert.ToChar(Console.ReadLine());
+            switch (op)
+            {
+                case '+':
+                    res = s1 + s2;
+                    break;
+                case '-':
+                    res = s1 - s2;
+                    break;
+                case '*':
+                    res = s1 * s2;
+                    break;
+                case '/':
+                    res = s1 / s2;
+                    break;
+                default:
+                    Console.WriteLine("Invalid operator");
+                    break;
+
+            }
+            Console.WriteLine("Result = " + res);
+            Console.ReadLine();
+
+        }
+    }
+}
