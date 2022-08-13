1876fefb22218bb65eff398d68ff107893b92fc5
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 18:40:27 +0530

    Program using float dt.

---
+++ Csharp/PracticeProgram/Program27.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrgmUsingFloatDt
+{
+    internal class FloatDatatype
+    {
+        static void Main(string[] args)
+        {
+            float b1;
+            Console.WriteLine("Enter Float number : ");
+            b1 = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("float entered = " + b1);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
