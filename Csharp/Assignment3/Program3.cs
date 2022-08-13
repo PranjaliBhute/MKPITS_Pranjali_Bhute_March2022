cd9f6a4df158b9fe60242a2da40cbb18336685db
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 12:47:15 +0530

    Program of prime number using do while loop.

---
+++ Csharp/Assignment3/Program3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNoPrgmUsingDoWhileLoop
+{
+    internal class PrimeNumber
+    {
+        static void Main(string[] args)
+        {
+            int num, i, flag = 0;
+            Console.WriteLine("Enter number : ");
+            num = Convert.ToInt32(Console.ReadLine());
+            i = 1;
+            do
+            {
+                if (num % i == 0)
+                {
+                    flag = flag + 1;
+                }
+                i++;
+            } while (i <= num);
+            if (flag == 2)
+            {
+                Console.WriteLine("Entered number is prime.");
+            }
+            else
+            {
+                Console.WriteLine("Entered number is not prime.");
+            }
+            Console.ReadLine();
+         }
+    }
+}
+
+
+    
+
