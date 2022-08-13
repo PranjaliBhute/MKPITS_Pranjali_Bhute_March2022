c7f4a342a33a7440bfc8ce3c1b0cae3e3d73138f
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 18:51:17 +0530

    Print 1 to 10 using while loop.

---
+++ Csharp/PracticeProgram/Program33.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print1to10UsingWhileLoop
+{
+    internal class Whileloopprgm
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            while (n <= 10)
+            {
+                Console.WriteLine(n);
+                n++;
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
