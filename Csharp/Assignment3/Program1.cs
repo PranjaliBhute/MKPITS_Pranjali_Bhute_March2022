24f32f1eb3ad5284d195b4f9c9d188b6b2b45301
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 12:41:00 +0530

    Program of factorial using do while loop.

---
+++ Csharp/Assignment3/Program1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactPrgmUsingDoWhileLoop
+{
+    internal class FactorialProgram
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int fact = 1;
+            Console.WriteLine("Enter number : ");
+            num = Convert.ToInt32(Console.ReadLine());
+            int i = num;
+            do
+            {
+                fact = fact * i;
+                i--;
+            } while (i > 0);
+            Console.WriteLine("Fact = " + fact);
+            Console.ReadLine();
+        }
+    }
+}
