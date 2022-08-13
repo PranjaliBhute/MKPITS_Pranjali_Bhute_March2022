bae86f691193d662dfcd137bcefc86529e33e578
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 17:57:20 +0530

    Addition program.

---
+++ Csharp/PracticeProgram/Program7.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Enter number1 : ");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            int res = a + b;
+            Console.WriteLine("Addition of two numbers = "+ res);
+            Console.ReadLine();
+
+
+        }
+    }
+}
