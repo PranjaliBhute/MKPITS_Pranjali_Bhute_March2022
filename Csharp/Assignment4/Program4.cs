d3ab155df8a21472f8d0fd656a8d78f7341389ca
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 15:26:18 +0530

    Print table using method.

---
+++ Csharp/Assignment4/Program4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintTableUsingMethod
+{
+    internal class ProgramOfPrintTable
+    {
+        static void PrintTable(int n)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(" {0} * {1} = {2} ", n, i, n * i);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int num;
+            Console.Write("Enter number : ");
+            num = Convert.ToInt32(Console.ReadLine());
+            PrintTable(num);
+            Console.ReadLine();
+        }
+
+    }
+}
