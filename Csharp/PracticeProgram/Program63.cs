9b20e09c110665788f19c79f0a96029460951163
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 23:40:05 +0530

    Program of student using methods.

---
+++ Csharp/PracticeProgram/Program63.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramOfStudentUsingMethods
+{
+    class Student
+    {
+        int rollno;
+        string name;
+
+        public void getstudentdata()
+        {
+            rollno = 11;
+            name = "Saanvi";
+        }
+        public void getstudentdata(int rollno, string name)
+        {
+            this.rollno = rollno;
+            this.name = name;
+        }
+        public void getstudentdata(string name, int rollno)
+        {
+            this.rollno = rollno;
+            this.name = name;
+        }
+        public void displaydata()
+        {
+            Console.WriteLine("rollno = " + rollno);
+            Console.WriteLine("name = " + name);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student S = new Student();
+            S.getstudentdata();
+            S.displaydata();
+            S.getstudentdata(11, "Saanvi");
+            S.displaydata();
+            S.getstudentdata("Saanvi", 11);
+            S.displaydata();
+            Console.ReadLine();
+        }
+    }
+}
