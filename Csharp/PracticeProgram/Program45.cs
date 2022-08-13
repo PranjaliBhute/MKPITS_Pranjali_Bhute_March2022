ebb14a760d0455da13cc27b099e12e1339db03b5
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 19:53:26 +0530

    Program of Employee.

---
+++ Csharp/PracticeProgram/Program45.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    class Employee
+    {
+        public int empno;
+        public string empname;
+        public float salary;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.empno = 21;
+            emp.empname = "Pranjali";
+            emp.salary = 20000.0f;
+            Console.WriteLine("Employee number : " + emp.empno);
+            Console.WriteLine("Employee name : " + emp.empname);
+            Console.WriteLine("Employee salary : " + emp.salary);
+            Console.ReadLine();
+
+        }
+    }
+}
