da40dea2a7fce7401b86fd390898effe89f5f380
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 18:48:07 +0530

    Print 1 to 10 using do while loop.

---
+++ Csharp/PracticeProgram/Program31.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print1to10UsingDoWhileLoop
+{
+    internal class DoWhileLoopPgrm
+    {
+        static void Main(string[] args)
+        {
+            int n = 1;
+            do
+            {
+                Console.WriteLine(n);
+                n++;
+            } while (n <= 10);
+            Console.ReadLine();
+        }
+    }
+}
