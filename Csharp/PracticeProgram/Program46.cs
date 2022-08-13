074dfba84049644e2ee709fc60947ac394b71083
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 19:57:21 +0530

    Employee_Accept value from user.

---
+++ Csharp/PracticeProgram/Program46.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_AcptValFrmUser
+{
+    class Employee
+    {
+        public int empno;
+        public string empname;
+        public float salary;
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            Console.WriteLine("Enter employee number : ");
+            emp.empno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter employee name : ");
+            emp.empname = Console.ReadLine();
+            Console.WriteLine("Enter employee salary : ");
+            emp.salary = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("empno = " + emp.empno);
+            Console.WriteLine("empname = " + emp.empname);
+            Console.WriteLine("salary = " + emp.salary);
+            Console.ReadLine();
+
+        }
+    }
+}
