bf7ca4d4875218838abec66a4c706233f190a9db
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 15:51:55 +0530

    Accept number print table.

---
+++ Csharp/PracticeProgram/Program6.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcptNoPrintTable
+{
+    internal class TableProgram
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int res = 1;
+            Console.WriteLine("Enter number : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= 10; i++)
+            {
+                res = n * i;
+                Console.WriteLine(" {0} * {1} = {2} ", n, i, res);
+            }
+            Console.ReadLine();
+        }
+    }
+}
