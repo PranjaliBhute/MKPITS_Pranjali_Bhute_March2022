66689d23ce9ba3acf2872e91f8023cb22b0b74b0
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 23:36:38 +0530

    Program.

---
+++ Csharp/PracticeProgram/Program62.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramOfStudent
+{
+    class Student
+    {
+        public int rno;
+        public void getdata(int rno)
+        {
+            this.rno = rno;
+        }
+        public void display()
+        {
+            Console.WriteLine("rno = " + rno);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student S = new Student();
+            S.getdata(11);
+            S.display();
+            Console.ReadLine();
+        }
+    }
+}
