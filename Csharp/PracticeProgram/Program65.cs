b151cbc70cfe96d9602e8e19bdf3e0c4b28790ef
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 23:45:13 +0530

    Protected access specifier.

---
+++ Csharp/PracticeProgram/Program65.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtectedAssessSpecifier
+{
+    class Student
+    { 
+       protected string name;
+
+        public void getdata()
+        {
+            name = "Saanvi";
+            Console.WriteLine("name = " + name);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student S = new Student();
+            S.getdata();
+            Console.ReadLine();
+        }
+    }
+}
