01ba09ab674068d2f8219e23b20797c17f78d296
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 18:46:08 +0530

    Program using ushort datatype.

---
+++ Csharp/PracticeProgram/Program30.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prgmUsingUshortDtype
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             ushort b1;
+            Console.WriteLine("Enter ushort number : ");
+            b1 = Convert.ToUInt16(Console.ReadLine());
+            Console.WriteLine("ushort entered = " + b1);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
