c60c21310e65061062089269c1fd4b09c74dc3bf
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 18:42:16 +0530

    Program using long dt.

---
+++ Csharp/PracticeProgram/Program28.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrgmUsingLongDt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           long b1;
+            Console.WriteLine("Enter long number : ");
+            b1 = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Long entered = " + b1);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
