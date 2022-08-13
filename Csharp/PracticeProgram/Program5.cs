3cb2a0efc174336bc2054db584d85206bbec2feb
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 15:48:46 +0530

    Accept name and print.

---
+++ Csharp/PracticeProgram/Program5.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcptNameAndPrint
+{
+    internal class PrintName
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("Enter your name : ");
+            name = Console.ReadLine();
+            Console.WriteLine("Name entered = " + name);
+            Console.ReadLine();
+        }
+    }
+}
