5d89eb406d3e95e248396818d061c7e52d7790e2
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 20:08:35 +0530

    Program of same name as classmenbers name.

---
+++ Csharp/PracticeProgram/Program50.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SameNameAsClassmembersName
+{
+    class Employee
+    {
+        int empno;
+        string empname;
+        float salary;
+
+        public void getdata(int empno,string empname,float salary)
+            {
+                 this.empno = empno;
+                 this.empname = empname;
+                 this.salary = salary;
+            }
+    public void displaydata()
+    {
+        Console.WriteLine("empno = " + empno);
+        Console.WriteLine("empname = " + empname);
+        Console.WriteLine("salary = " + salary);
+    }
+    }
+    internal class Program
+    {
+    static void Main(string[] args)
+    {
+        Employee emp = new Employee();
+        Console.WriteLine("Enter employee number : ");
+        int eno = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter employee name : ");
+        string en = Console.ReadLine();
+        Console.WriteLine("Enter salary : ");
+        float sal = Convert.ToSingle(Console.ReadLine());
+
+        emp.getdata(eno, en, sal);
+        emp.displaydata();
+        Console.ReadLine();
+    }
+    }
+}
