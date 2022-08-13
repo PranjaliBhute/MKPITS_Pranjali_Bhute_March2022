78881f7863e29fa8e722fd5caf8b425eb976f8cd
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 23:03:09 +0530

    Program of Book.

---
+++ Csharp/PracticeProgram/Program52.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book
+{
+    class Book
+    {
+        int bookid;
+        string title, author;
+        int price;
+
+        public Book()
+            {
+            bookid = 1;
+            title = "Oracle";
+            author = "james";
+            price = 200;
+            }
+        public void display()
+        {
+            Console.WriteLine("Bookid = " + bookid);
+            Console.WriteLine("Book title = " + title);
+            Console.WriteLine("Book author = " + author);
+            Console.WriteLine("Price = " + price);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book b = new Book();
+            b.display();
+            Console.ReadLine();
+        }
+    }
+}
