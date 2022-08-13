7c22c637936acfa2b4d494aef0a8abb64fbe6bd2
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 12:20:55 +0530

    Program of display emp_hra_da_total salary.

---
+++ Csharp/Assignment2/Program10.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrgmOfDisplayEmpHraDaTotalSal
+{
+    internal class DisplayTotalSalary
+    {
+        static void Main(string[] args)
+        {
+            int empno, basicsal;
+            Console.WriteLine("Enter Employee number : ");
+            empno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter basic salary : ");
+            basicsal = Convert.ToInt32(Console.ReadLine());
+            float hra = 0;
+            float da = 0;
+            switch (basicsal)
+            {
+                case 25000:
+                    hra = basicsal * 0.32f;
+                    da = basicsal * 0.45f;
+                    break;
+                default:
+                    Console.WriteLine("Invalid");
+                    break;
+                
+            }
+            float total = basicsal + hra + da;
+            Console.WriteLine("empno " + empno);
+            Console.WriteLine("hra " + hra);
+            Console.WriteLine("da " + da);
+            Console.WriteLine("total salary " + total);
+            Console.ReadLine();
+         }
+    }
+}
