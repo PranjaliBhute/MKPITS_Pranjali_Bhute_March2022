9fb48830ae6cb80935a913e9c91bbc96285ff2e1
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 15:14:35 +0530

    Program of 3 by 3 array addition.

---
+++ Csharp/Assignment4/Program2.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrgmOf3by3ArrayAddition
+{
+    internal class AddInThirdArrayProgram
+    {
+        static void Main(string[] args)
+        {
+            int[,] num1 = new int[3, 3];
+            int[,] sum = new int[3, 3];
+            Console.WriteLine("Enter Array Element : ");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    num1[i, j] = Convert.ToInt16(Console.ReadLine());
+                }
+            }
+            Console.WriteLine();
+            int[,] num2 = new int[3, 3];
+            Console.WriteLine("Enter Array Element : ");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    num2[i, j] = Convert.ToInt16(Console.ReadLine());
+                }
+            }
+            Console.WriteLine();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    sum[i, j] = num1[i, j] + num2[i, j];
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(sum[i, j] + "\t");
+                }
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
