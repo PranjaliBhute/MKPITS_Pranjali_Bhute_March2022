5378c87efc984721dc3808c485ca20e8b4b18908
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 20:04:46 +0530

    Program of getdata and showdata.

---
+++ Csharp/PracticeProgram/Program49.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrgmOfGetdataAndShowdata
+{
+    class Employee
+    {
+        public int empno;
+        public string empname;
+        public float salary;
+      
+        public void getdata()
+        {
+            Console.WriteLine("Enter employee no : ");
+            empno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter employee name : ");
+            empname = Console.ReadLine();
+            Console.WriteLine("Enter employee salary : ");
+            salary = Convert.ToSingle(Console.ReadLine());
+
+        }
+        public void displaydata()
+        {
+            Console.WriteLine("empno = " + empno);
+            Console.WriteLine("empname = " + empname);
+            Console.WriteLine("salary  = " + salary);
+        }
+
+    }
+    internal class Program
+    {
+         static void Main(string[] args)
+        {
+           Employee emp = new Employee();
+
+            emp.getdata();
+            emp.displaydata();
+            Console.ReadLine();
+        }
+    }
+}
