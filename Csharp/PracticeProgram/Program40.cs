5b4868f0150e5c031a1efa3c186f855f152c0fc2
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 19:06:39 +0530

    Program using sbyte.

---
+++ Csharp/PracticeProgram/Program40.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramUsingSbyte
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte b1;
+            Console.WriteLine("Enter sbyte : ");
+            b1 = Convert.ToSByte(Console.ReadLine());
+            Console.WriteLine("Sbyte entered = " + b1);
+            Console.ReadLine();
+
+        }
+    }
+}
