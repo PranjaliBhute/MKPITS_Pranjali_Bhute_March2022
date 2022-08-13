e95b05e4537ea0c103f1483f7e645b1db6e8df4b
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 23:57:56 +0530

    Example of single level inheritance.

---
+++ Csharp/PracticeProgram/Program71.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleOfSingleLevelInheritance
+{
+    class Employee
+    {
+        protected int salary;
+    }
+    class Programmer : Employee 
+    {
+        int bonus;
+
+        public void getdata(int salary, int bonus)
+        {
+            this.salary = salary;
+            this.bonus = bonus;
+        }
+        public void displaydata()
+        {
+            Console.WriteLine("salary = " + salary);
+            Console.WriteLine("bonus = " + bonus);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Programmer P = new Programmer();
+            P.getdata(25000, 20000);
+            P.displaydata();
+            Console.ReadLine();
+        }
+    }
+}
