a0a0ba2a75f4c51536df077f484c85b516d68a27
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 23:33:52 +0530

    Program of student using internal specifier.

---
+++ Csharp/PracticeProgram/Program61.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgmOfStudentUsingInternalSpecifier
+{
+    class Student
+    {
+        internal int rollno;
+        internal string name;
+
+        public void displaydata()
+        {
+            Console.WriteLine("rollno = " + rollno);
+            Console.WriteLine("name = " + name);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student S = new Student();
+            S.rollno = 11;
+            S.name = "Saanvi";
+            S.displaydata();
+            Console.ReadLine();
+        }
+    }
+}
