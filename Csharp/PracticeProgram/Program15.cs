812151cf66b594deda58f5ee4a73302239455ad5
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 18:14:21 +0530

    Local and class variable program.

---
+++ Csharp/PracticeProgram/Program15.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalAndClassVariableProgram
+{
+    internal class Program
+    {
+        static int gnum = 20;
+        static void display()
+        {
+            int num = 5;
+            Console.WriteLine("num inside method = " + num);
+            Console.WriteLine("gnum inside method = " + gnum);
+        }
+        static void Main(string[] args)
+        {
+            display();
+            Console.WriteLine("gnum inside main = " + gnum);
+            Console.ReadLine();
+        }
+    }
+}
