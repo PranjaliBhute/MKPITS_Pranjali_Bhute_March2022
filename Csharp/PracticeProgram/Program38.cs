59dfa4a617bcbb48db7362780e072db1504b58ca
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 19:01:16 +0530

    Program of create 2d array 3 by 3.

---
+++ Csharp/PracticeProgram/Program38.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramOfCreate2dArray3by3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] num = new int[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.WriteLine("Enter number : ");
+                    num[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(num[i, j] + "\t");
+                }
+                Console.WriteLine();
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
