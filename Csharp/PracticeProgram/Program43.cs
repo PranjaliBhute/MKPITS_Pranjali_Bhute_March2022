38b830249282a7ba8869ea9764ca26b989c389c9
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 19:15:05 +0530

    String array using for each loop.

---
+++ Csharp/PracticeProgram/Program43.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringArrayUsingForEachLoop
+{
+    internal class StringArray
+    {
+        static void Main(string[] args)
+        {
+            string[] num = new string[3];
+            num[0]= "Pranjali";
+            num[1]= "Saanvi";
+            num[2]= "Vaibhav";
+            foreach (string i in num)
+            {
+                Console.WriteLine(i);
+                Console.ReadLine();
+            }
+        }
+    }
+}
