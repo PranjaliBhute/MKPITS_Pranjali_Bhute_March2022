4e53b9e94b116d34e63b6ab22c7620856252a945
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 14:52:32 +0530

    Print prime number 1 to 20 using while loop.

---
+++ Csharp/Assignment3/Program5.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintPrimeNo1to20UsingWhileLoop
+{
+    internal class PrintPrimeProgram
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            for (i = 1; i < 20; i++)
+            {
+                for (j = 2; j <= i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        break;
+                    }
+                }
+                if (i == j)
+                {
+                    Console.WriteLine("Prime number between 1 to 20 = " + i);
+                }
+            }
+            Console.ReadLine();
+
+         }
+        
+    }
+}
+
+
+        
+    
+
