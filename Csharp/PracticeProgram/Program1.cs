6d4bfc22f4524e04fdd8cdbdcc78c6475e20262f
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 15:40:57 +0530

    Accept age and print.

---
+++ Csharp/PracticeProgram/Program1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcceptAgeAndPrint
+{
+    internal class CheckEligibility
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            Console.WriteLine("Enter age : ");
+            age = Convert.ToInt32(Console.ReadLine());
+            if (age < 18)
+                Console.WriteLine("Not eligible to vote.");
+            else
+                Console.WriteLine("Eligible to vote.");
+            Console.ReadLine();
+
+        }
+    }
+}
