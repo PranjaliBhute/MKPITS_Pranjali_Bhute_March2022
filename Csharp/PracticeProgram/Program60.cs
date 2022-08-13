4ffcba17d29e1af478f5a4a2abbeb7a1fe8353a6
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 23:30:28 +0530

    Example of inheritance.

---
+++ Csharp/PracticeProgram/Program60.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleOfInheritance
+{
+    class Person
+    {
+        protected string name;
+    }
+    class Student : Person 
+    {
+        public void getdata()
+        {
+            name = "Saanvi";
+            Console.WriteLine("name = " + name);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student S = new Student();
+            S.getdata();
+            Console.ReadLine();
+        }
+    }
+}
