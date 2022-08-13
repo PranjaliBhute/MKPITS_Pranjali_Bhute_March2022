023f9ae1fed8c91d504a82c89ffbe92189ee7251
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 18:05:00 +0530

    Datatype in csharp.

---
+++ Csharp/PracticeProgram/Program10.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatatypeInCsharp
+{
+    internal class DatatypeInCsharp
+    {
+        static void Main(string[] args)
+        {
+            byte b1;
+            Console.WriteLine("Enter byte : ");
+            b1 = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine("Byte entered = " + b1);
+            Console.ReadLine();
+        }
+    }
+}
