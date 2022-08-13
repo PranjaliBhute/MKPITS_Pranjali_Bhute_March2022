27312313e1232cbd8b49c30e94e97ee3ddc24cca
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-13 23:47:12 +0530

    Protected internal access specifier.

---
+++ Csharp/PracticeProgram/Program66.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtectedInternalAccessSpecifier
+{
+    class ProtectedTest
+    {
+        protected internal string name = "Saanvi";
+        protected void Msg(string msg)
+        {
+            Console.WriteLine("Hello" + msg);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProtectedTest PT = new ProtectedTest();
+            PT.name = "Saanvi";
+            Console.WriteLine(PT.name);
+            Console.ReadLine();
+        }
+    }
+}
