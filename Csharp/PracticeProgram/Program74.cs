5834d09c87a6bd07f8026d36d16ca9f8184beaf0
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-14 17:43:52 +0530

    Method with parameter and return value.

---
+++ Csharp/PracticeProgram/Program74.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodWithParaAndReturnValue
+{
+    internal class SquareProgram
+    {
+        static int square(int num)
+        {
+            int square = num * num;
+            return square;
+        }
+        static void Main(string[] args)
+        {
+            int num = 5;
+            int res = square(num);
+            Console.WriteLine("Square = " + res);
+            Console.ReadLine();
+        }
+    }
+}
