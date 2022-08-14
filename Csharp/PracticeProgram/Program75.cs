96c2baab4984878bcee766261c62b619ee5844a5
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-14 17:46:05 +0530

    Method with parameter no returntype.

---
+++ Csharp/PracticeProgram/Program75.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodWithParaNoReturntype
+{
+    internal class Program
+    {
+        static void square(int num)
+        {
+            int square = num * num;
+            Console.WriteLine("Square = " + square);
+        }
+
+        static void Main(string[] args)
+        {
+            int num = 5;
+            square(num);
+            Console.WriteLine("Bye");
+            Console.ReadLine();
+        }
+    }
+}
