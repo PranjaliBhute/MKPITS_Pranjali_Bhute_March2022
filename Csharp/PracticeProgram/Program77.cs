c9572dfe50ee5883f331e679ec7f74dd1e698e1d
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-14 17:51:20 +0530

    Using method sqaure program.

---
+++ Csharp/PracticeProgram/Program77.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingMethodSquarePrgm
+{
+    internal class SquareProgram
+    {
+        static void square()
+        { 
+        int num = 5;
+        int square = num * num;
+        Console.WriteLine("Square = " + square);
+        }
+        static void Main(string[] args)
+        {
+            square();
+            Console.WriteLine("Bye");
+            Console.ReadLine();
+        }
+    }
+}
