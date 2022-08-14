86bc776db07e05dd6ddd669a0460c70880bff092
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-14 17:48:07 +0530

    No parameter no returntype program.

---
+++ Csharp/PracticeProgram/Program76.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoParameterNoReturnProgram
+{
+    internal class Program
+    {
+        static void display()
+        {
+           Console.WriteLine("Hello from display method");
+        }
+        static void Main(string[] args)
+        {
+            display();
+            Console.WriteLine("Bye");
+            Console.ReadLine();
+        }
+    }
+}
