dcd8448cc411777ade3ff88c4e04998bbf2c9d73
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-15 21:53:00 +0530

    Entered number equal or not.

---
+++ Practice Assignment Questions/Program1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Assignment_Questions_1
+{
+    internal class NumEqualOrNot
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Enter First Number : ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Second Number : ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                if (num1 == num2)
+                {
+                    Console.WriteLine("Given number are equal.");
+                }
+                else
+                {
+                    Console.WriteLine("Given number are not equal.");
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+
+
+    
+
