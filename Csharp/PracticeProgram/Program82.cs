1d1eb12e21b0fb6ae66878c7c43292596e2f6c86
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-15 18:30:46 +0530

    Aggregation(HAS -A relationship).

---
+++ Csharp/PracticeProgram/Program82.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aggregation_HAS__A_relationship_
+{
+    public class Address
+    {
+        public string addressLine, city, state;
+
+        public Address(string addressLine, string city, string state)
+        {
+            this.addressLine = addressLine;
+            this.city = city;
+            this.state = state;
+        }
+    }
+    public class Employee
+    {
+        public int id;
+        public string name;
+        public Address address;
+
+        public Employee(int id, string name, Address address)
+        {
+            this.id = id;
+            this.name = name;
+            this.address = address;
+        }
+        public void display()
+        {
+            Console.WriteLine(id + "" + name + "" + address.addressLine + "" + address.city + "" + address.state);
+        }
+
+    }
+    internal class TestAggregation
+    {
+        static void Main(string[] args)
+        {
+            Address Add = new Address("G-13, Sec-3", "Noida","UP");
+            Employee emp = new Employee(1, "Saanvi", Add);
+            emp.display();
+            Console.ReadLine();
+        }
+    }
+}
