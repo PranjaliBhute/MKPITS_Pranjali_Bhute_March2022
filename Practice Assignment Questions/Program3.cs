9ca038ab47ec788be2b04d121d1dcfaeaa9c4331
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-15 22:28:49 +0530

    Given number is positive or negative.

---
+++ Practice Assignment Questions/Program3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Assignment_Question_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number : ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num == 0)
+            {
+                Console.WriteLine("Entered number is zero.");
+            }
+            else if (num > 0)
+            {
+                Console.WriteLine("Entered number is positive.");
+            }
+            else
+            {
+                Console.WriteLine("Entered number is negative.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
