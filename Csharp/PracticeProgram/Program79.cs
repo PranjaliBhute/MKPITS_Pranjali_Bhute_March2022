c493697ea382ea9caec020949a588abe2b79b158
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-15 15:03:33 +0530

    Example of protected.

---
+++ Csharp/PracticeProgram/Program79.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_of_Protected
+{
+    class Person
+    {
+        string name;
+        protected void getpersondata(string name)
+        {
+            this.name = name;
+            Console.WriteLine("name = " + name);
+        }
+            
+    }
+    class Employee : Person
+    {
+        int empno;
+        internal void getemployeedata(int empno, string name)
+        {
+            getpersondata(name);
+            this.empno = empno;
+            Console.WriteLine("empno = " + empno);
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.getemployeedata(11, "Saanvi");
+            Console.ReadLine();
+        }
+    }
+}
