f1fff84092bd0ab7e5bd2f47b65d760f5845a40c
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-16 18:43:17 +0530

    Runtime polymorphism with datamember.

---
+++ Csharp/Assignment5/Program12.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuntimePolymorphismWithDataMembers
+{
+    class Animal
+    {
+        public string color = "White";
+    }
+    class Dog : Animal 
+    {
+        public string color = "Black";
+    }
+    internal class TestPolymorphism
+    {
+        static void Main(string[] args)
+        {
+            Animal d = new Dog();
+            Console.WriteLine(d.color);
+            Console.ReadLine();
+        }
+    }
+}
