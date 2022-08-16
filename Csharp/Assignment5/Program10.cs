f6410a0f026c413a7eaacde0c6f4ef108256b9e8
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-16 18:39:38 +0530

    Runtime polymorphism example.

---
+++ Csharp/Assignment5/Program10.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunTimePolymorphismExample
+{
+    public class Animal
+    {
+        public virtual void eat()
+        {
+            Console.WriteLine("Eating.....");
+        }
+    }
+    public class Dog : Animal
+    {
+        public override void eat()
+        {
+            Console.WriteLine("Eating bread...");
+        }
+    }
+    internal class TestPolymorphism
+    {
+        static void Main(string[] args)
+        {
+            Animal A = new Dog();
+            A.eat();
+            Console.ReadLine();
+        }
+    }
+}
