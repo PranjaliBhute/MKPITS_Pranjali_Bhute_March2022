a371bb3edf91e621b67fa9b7b5698715b27bc403
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-16 18:46:18 +0530

    Practice Assignment Question 5.

---
+++ Practice Assignment Questions/Program5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Assignment_Question_5
+{
+    internal class EligibleForVoteOrNot
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age : ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (age >= 18)
+            {
+                Console.WriteLine("Congratulation! You are eligible for casting your vote.");
+            }
+            else
+            {
+                Console.WriteLine("You are not eligible for casting your vote.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
