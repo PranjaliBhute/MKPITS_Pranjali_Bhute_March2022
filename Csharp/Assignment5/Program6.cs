13bb7ddfd59c2e36aa664090a308be0db8eed2fa
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-16 18:30:39 +0530

    Example of base keyword.

---
+++ Csharp/Assignment5/Program6.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleOfBaseKeyword
+{
+    public class Animal
+    {
+        public void eat()
+        {
+            Console.WriteLine("Eating.....");
+        }
+    }
+    public class Dog : Animal 
+    {
+        public void eat()
+        {
+            base.eat();
+            Console.WriteLine("Eating bread.....");
+        }
+    }
+    internal class TestBase
+    {
+        static void Main(string[] args)
+        {
+            Dog g = new Dog();
+            g.eat();
+            Console.ReadLine();
+        }
+    }
+}
