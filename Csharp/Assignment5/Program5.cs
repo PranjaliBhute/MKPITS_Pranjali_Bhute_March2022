03135ba571852c735bd1f6cf7eb541cd07e17993
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-16 18:29:11 +0530

    Calling base class method.

---
+++ Csharp/Assignment5/Program5.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingBaseClassMethod
+{
+    class Person
+    {
+        string name = "Mkpits";
+        public void display()
+        {
+            Console.WriteLine("Base class fiels name = " + name);
+        }
+    }
+    class Student : Person 
+    {
+        string name = "Roshan";
+        public void display()
+        {
+            base.display();
+            Console.WriteLine("Child class field name = " + name);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student S = new Student();
+            S.display();
+            Console.ReadLine();
+        }
+      
+    }
+}
