71b7aa3b1fc810ecaccb7d2c9a48459c47ec57fa
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-16 18:41:19 +0530

    Runtime polymorphism example 2.

---
+++ Csharp/Assignment5/Program11.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuntimePolymorphismExample2
+{
+    public class Shape
+    {
+        public virtual void draw()
+        {
+            Console.WriteLine("Drawing....");
+        }
+    }
+    public class Rectangle : Shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("Drawing rectangle....");
+        }
+    }
+    public class Circle : Shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("Drawing circle....");
+        }
+    }
+
+    internal class TestPolymorphism
+    {
+        static void Main(string[] args)
+        {
+            Shape S;
+            S = new Shape();
+            S.draw();
+            S = new Rectangle();
+            S.draw();
+            S = new Circle();
+            S.draw();
+            Console.ReadLine();
+        }
+    }
+}
