b6bc01fecb8fe7649d33dae09b24af056459144b
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-16 18:22:38 +0530

    AccessesTheFieldOfBaseClass.

---
+++ Csharp/Assignment5/Program1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessesTheFieldOfBaseClass
+{
+    class Person
+    {
+        public string name = "Mkpits";
+    }
+    class Student : Person
+    {
+        public string name = "Roshan";
+        public void display()
+        {
+            Console.WriteLine("name = " + name); 
+            Console.WriteLine("base class field name = " + base.name);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student S = new Student();
+            S.display();
+            Console.ReadLine();
+        }
+    }
+}
