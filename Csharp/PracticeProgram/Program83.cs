28f38b66f9e4a1902784566a1c080ea66a9390a8
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-17 22:52:47 +0530

    Abstract class of person.

---
+++ Csharp/PracticeProgram/Program83.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_class_of_person
+{
+    abstract class Person
+    {
+        public abstract void display();
+        public void showdata()
+        {
+            Console.WriteLine("Hello from showdata");
+        }
+    }
+    class Student : Person
+    {
+        public override void display()
+        {
+            Console.WriteLine("Hello from display method");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.display();
+            s.showdata();
+            Console.ReadLine();
+
+        }
+    }
+}
