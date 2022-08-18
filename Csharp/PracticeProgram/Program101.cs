0c02f48ad87478e25f24cd985a9f382750c03dd6
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-18 17:25:13 +0530

    Example of static constructor.

---
+++ Csharp/PracticeProgram/Program101.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_of_static_constructor
+{
+    public class Account
+    {
+        public int id;
+        public string name;
+        public static float rateofInterest;
+        public Account(int id, string name)
+        {
+            Console.WriteLine("Instance constructor is called ");
+            this.id = id;
+            this.name = name;
+        }
+        static Account()
+        {
+            Console.WriteLine("Static constructor is called ");
+            rateofInterest = 9.5f;
+        }
+        public void display()
+        {
+            Console.WriteLine(id + " " + name + " " + rateofInterest);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account a1 = new Account(1, "Saanvi");
+            Account a2 = new Account(2, "Pranjali");
+            a1.display();
+            a2.display();
+            Console.ReadLine();
+
+        }
+    }
+}
