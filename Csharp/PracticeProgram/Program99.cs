7f92c14ef4157996a690e4f61fe51ef3acbe857b
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-18 17:19:25 +0530

    Example of sealed class.

---
+++ Csharp/PracticeProgram/Program99.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_of_sealed_class
+{
+    sealed class Student
+    { }
+    class Parttimestudent : Student
+    { }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
