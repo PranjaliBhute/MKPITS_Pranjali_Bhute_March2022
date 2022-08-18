c9da82d549966e0a982d2cdf1ee6722315824af7
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-18 12:39:29 +0530

    Abstract class shape.

---
+++ Csharp/PracticeProgram/Program84.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_class_shape
+{
+    public abstract class Shape
+    {
+        public abstract void draw();
+    }
+    public class Rectangle : Shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("Drawing rectangle....");
+        }
+    }
+    public class Circle : Shape
+    {
+        public override void draw()
+        {
+            Console.WriteLine("Drawing circle....");
+        }
+    }
+    internal class TestAbstract
+    {
+        static void Main(string[] args)
+        {
+            Shape s;
+            s = new Rectangle();
+            s.draw();
+            s = new Circle();
+            s.draw();
+            Console.ReadLine();
+        }
+    }
+}
