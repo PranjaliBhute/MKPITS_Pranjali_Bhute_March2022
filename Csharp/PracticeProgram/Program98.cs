1fe9ed8a6f1f3c100936eca4a7617e2769a64724
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-18 17:17:37 +0530

    Example of static keyword.

---
+++ Csharp/PracticeProgram/Program98.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_of_ststic_keyword
+{
+    class Student
+    {
+        public static void display()
+        {
+            Console.WriteLine("Hello");
+        }
+        public void sayhi()
+        {
+            Console.WriteLine("Hi");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student.display();
+           // Student.sayhi();
+            Student S = new Student();
+            S.sayhi();
+            Console.ReadLine();
+        }
+    }
+}
