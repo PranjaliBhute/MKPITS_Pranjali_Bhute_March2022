4d50e2831c1a41756f27a76b7c5ac7e62007e5f0
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-18 13:16:21 +0530

    Example of interface bank.

---
+++ Csharp/PracticeProgram/Program94.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleOfInterfaceBank
+{
+    interface Bank
+    {
+        string deposit(int actno, int amount);
+    }
+    class Saving : Bank
+    {
+        int actno;
+        int balance = 1000;
+
+        public string deposit(int actno, int amount)
+        {
+            this.actno = actno;
+            balance = balance + amount;
+            return "Deposited successfully, balance is " + balance;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank b;
+            b = new Saving();
+            String str = b.deposit(1, 200);
+            Console.WriteLine(str);
+            Console.ReadLine();
+        }
+    }
+}
