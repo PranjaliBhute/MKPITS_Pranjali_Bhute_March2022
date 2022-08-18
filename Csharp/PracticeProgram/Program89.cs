96711548cf67f0ff8e16f8fc11721ccae9d5bffb
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-18 13:03:23 +0530

    Example of Encapsulation.

---
+++ Csharp/PracticeProgram/Program89.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_of_Encapsulation
+{
+    class Employee
+    { 
+         public string empname { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.empname = "ravi";
+            Console.WriteLine("empname " + emp.empname);
+            Console.ReadLine();
+        }
+    }
+}
