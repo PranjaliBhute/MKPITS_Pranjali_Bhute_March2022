f1a73098180251875408a914ede42b35dba36c59
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-18 17:27:27 +0530

    Sealed class student.

---
+++ Csharp/PracticeProgram/Program102.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sealed_class_student
+{
+    sealed class Student
+    {
+        public string name;
+    }
+    class Parttime : Student
+    { }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
