1d92627baf07641fa94c34d428bd9110cb6dc4b8
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-18 13:13:29 +0530

    Example of abstract class.

---
+++ Csharp/PracticeProgram/Program92.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleOfAbstractClass
+{
+    abstract class Account
+    {
+        public abstract void deposit();
+
+        public void showbalance()
+        {
+            Console.WriteLine("Balance Method ");
+        }
+    }
+    class Saving : Account
+    {
+        public override void deposit()
+        {
+            Console.WriteLine("Hello from saving deposit method");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account act = new Saving();
+            act.deposit();
+            act.showbalance();
+            Console.ReadLine();
+        }
+    }
+}
