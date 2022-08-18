16884ab73eed94debfaecbf3e41917f7c45b3215
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-18 23:13:23 +0530

    To find the largest of three numbers.

---
+++ Practice Assignment Questions/Program8.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Assignment_Question_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 1st number : ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 2nd number : ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 3rd number : ");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            if (num1 > num2 && num1 > num3)
+            {
+                Console.WriteLine("The 1st number is the greatest among three.");
+            }
+            else if (num2 > num1 && num2 > num3)
+            {
+                Console.WriteLine("The 2nd number is the greatest among three.");
+            }
+            else 
+            {
+                Console.WriteLine("The 3rd number is the greatest among three.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
