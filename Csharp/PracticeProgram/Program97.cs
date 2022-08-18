b805a3089339b010bb01501f0b7f33d93211014b
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-18 17:06:20 +0530

    Abstract class person.

---
+++ Csharp/PracticeProgram/Program97.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstrct_class_person
+{
+    abstract class Person
+    {
+        public string name;
+    }
+    class Student : Person 
+    {
+        public int rollno;
+    }
+    sealed class Parttimestudent : Student 
+    {
+        public int hours;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Person p = new Person(); //error
+            Parttimestudent p = new Parttimestudent();
+            p.rollno = 22;
+            p.name = "Saanvi";
+            p.hours = 5;
+            Console.WriteLine("rollno : " + p.rollno);
+            Console.WriteLine("name : " + p.name);
+            Console.WriteLine("hours " + p.hours);
+            Console.ReadLine();
+
+
+        }
+    }
+}
