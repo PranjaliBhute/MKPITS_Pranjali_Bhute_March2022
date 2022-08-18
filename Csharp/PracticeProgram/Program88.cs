01e5cfb7184a2173b7238e49beecd57df563ee89
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-18 12:52:13 +0530

    Example of access specifier.

---
+++ Csharp/PracticeProgram/Program88.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_Of_Access_Specifier
+{
+    class Student
+    { 
+      public string Id { get; set; }
+      public string Name { get; set; }
+      public string Email { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student S = new Student();
+            S.Id = "101";
+            S.Name = "Pranjali";
+            S.Email = "bhutepranjali@gmail.com";
+            Console.WriteLine("Id = " + S.Id);
+            Console.WriteLine("Name = " + S.Name);
+            Console.WriteLine("Email = " + S.Email);
+            Console.ReadLine();
+        }
+    }
+}
