b86ad7b7b299adf8ec99df487a2bb1c51eebd2d0
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-18 23:21:56 +0530

    Program to check whether an alphabet is a vowel or consonant.

---
+++ Practice Assignment Questions/Program16.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Assignment_Question_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter alphabet : ");
+            int ch = Convert.ToChar(Console.ReadLine());
+            if (ch == 'a')
+                Console.WriteLine("The alphabet is vowel.");
+            else
+            if (ch == 'e')
+                Console.WriteLine("The alphabet is vowel.");
+            else
+            if (ch == 'i')
+                Console.WriteLine("The alphabet is vowel.");
+            else
+            if (ch == 'o')
+                Console.WriteLine("The alphabet is vowel.");
+           else 
+           if (ch == 'u')
+                Console.WriteLine("The alphabet is vowel.");
+           else
+               Console.WriteLine("The alphabet is a consonant.");
+            
+               Console.ReadLine();
+        }
+    }
+}
