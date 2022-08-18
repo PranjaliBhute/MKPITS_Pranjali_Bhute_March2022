3dcd5b93555972ceb1af98cbf28f1ce3aee3851d
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-18 13:07:57 +0530

    Example of interface.

---
+++ Csharp/PracticeProgram/Program91.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_of_Interface
+{
+    interface Shape
+    {
+        void draw();
+    }
+    class Rectangle : Shape
+    {
+        public void draw()
+        {
+            Console.WriteLine("Draw method of rectangle class");
+        }
+    }
+    class Circle : Shape
+    {
+        public void draw()
+        {
+            Console.WriteLine("Draw method of circle class");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape s = new Rectangle();
+            s.draw();
+            s = new Circle();
+            s.draw();
+            Console.ReadLine();
+
+
+        }
+    }
+}
