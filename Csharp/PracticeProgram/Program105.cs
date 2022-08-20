67283ed6254f3f7233c88cc8dce9858772811ab0
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-20 21:08:45 +0530

    Example of exception handling.

---
+++ Csharp/PracticeProgram/Program105.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_of_Exception_Handling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 10;
+            int n = 0;
+            int res = num/n;//Error will come here and program terminates.
+            Console.WriteLine("res = " + res);
+            Console.WriteLine("Bye");
+            Console.ReadLine();
+        }
+    }
+}
