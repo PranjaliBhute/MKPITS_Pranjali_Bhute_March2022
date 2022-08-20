687ecc354ce3424015daa5734f349a790bbb78a4
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-20 21:07:18 +0530

    Example indexoutofrangeexception class.

---
+++ Csharp/PracticeProgram/Program104.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_indexoutofrangeexception_class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[3];
+            arr[0] = 22;
+            arr[1] = 33;
+            arr[2] = 44;
+            try
+            {
+                for (int i = 0; i <= 3; i++)
+                {
+                    Console.WriteLine("arr =" + arr[i]);
+                }
+            }
+            catch (IndexOutOfRangeException ee)
+            {
+                Console.WriteLine("Catch block is executing ");
+                Console.WriteLine(ee.ToString());
+            }
+            catch (Exception ee)
+            {
+                Console.WriteLine(ee.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("Finally block is executing ");
+
+            }
+
+            Console.WriteLine("Bye");
+            Console.ReadLine();
+        }
+    }
+}
