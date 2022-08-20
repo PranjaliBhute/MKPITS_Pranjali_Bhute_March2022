5a9df40eb875c1f40f9b29bac04e7d2baa91fa58
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-20 21:29:56 +0530

    Stringbuilder used append class.

---
+++ Csharp/PracticeProgram/Program206.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stringbuilder_used_append_class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Saanvi ");
+            sb.Append("Bhute");
+            Console.WriteLine(sb.ToString());
+            Console.WriteLine("Bye");
+            Console.ReadLine();
+
+        }
+    }
+}
