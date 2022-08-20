01596a28e9d020b7b16b3efb36851bbbae12323f
PranjaliBhute <bhutepranjali@gmail.com>
2022-08-20 21:14:00 +0530

    Example of outofmemoryexception class.

---
+++ Csharp/PracticeProgram/Program107.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_of_outofmemoryexception_class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Saanvi";
+            string subject = "Mathematics";
+            Console.WriteLine("Length of name = " + name.Length);
+            StringBuilder sb = new StringBuilder(name.Length, name.Length);
+            try
+            {
+                sb.Append(subject);
+            }
+            catch (Exception ee)
+            {
+                Console.WriteLine(ee.ToString());
+            }
+            Console.WriteLine("Bye");
+            Console.ReadLine();
+        }
+    }
+}
