116a6c339bff7af8e8d6c42527a4529342d183c1
PranjaliBhute <bhutepranjali@gmail.com>
2022-09-18 17:58:23 +0530

    Create table employee_IdentityExample

---
+++ SQL/SQL_practice_query/Identity_Example.sql
@@ -0,0 +1,12 @@
+create table employee(empid int primary key identity(1,1),epname varchar(20))
+insert into employee(epname) values('Pranjali')
+select * from employee
+insert  into employee(epname)values('Saanvi')
+select * from employee
+delete from employee where empid=1
+select * from employee
+insert into employee(epname)values('Vaibhav')
+delete from employee
+select * from employee
+insert into employee(epname)values('Komal')
+select * from employee
