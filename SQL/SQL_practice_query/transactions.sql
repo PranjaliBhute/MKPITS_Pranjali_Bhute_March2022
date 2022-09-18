4dea2031e5ee8699018807833fb41431dffda28d
PranjaliBhute <bhutepranjali@gmail.com>
2022-09-18 19:42:30 +0530

    Example of Transactions

---
+++ SQL/SQL_practice_query/transactions.sql
@@ -0,0 +1,39 @@
+CREATE TABLE Product (  
+     Product_id INT PRIMARY KEY,   
+     Product_name VARCHAR(40),   
+     Price INT,  
+     Quantity INT  
+    )  
+
+	INSERT INTO Product VALUES(111, 'Mobile', 10000, 10),  
+                              (112, 'Laptop', 20000, 15),  
+                              (113, 'Mouse', 300, 20),  
+                              (114, 'Hard Disk', 4000, 25),  
+                              (115, 'Speaker', 3000, 20);  
+
+select * from Product
+
+--Example of COMMIT Transaction
+
+-- Start a new transaction    
+    BEGIN TRANSACTION  
+ -- SQL Statements  
+     INSERT INTO Product VALUES(116, 'Headphone', 2000, 30)  
+     UPDATE Product SET Price = 450 WHERE Product_id = 113  
+     -- Commit changes   
+    COMMIT TRANSACTION  
+
+	
+select * from Product
+
+--Example of ROLLBACK Transaction
+
+    -- Start a new transaction    
+    BEGIN TRANSACTION  
+    -- SQL Statements  
+    UPDATE Product SET Price = 5000 WHERE Product_id = 114  
+    DELETE FROM Product WHERE Product_id = 116  
+
+	select * from Product
+
+
