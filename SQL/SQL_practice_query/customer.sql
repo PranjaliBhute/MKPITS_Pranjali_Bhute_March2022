77df2e1aa2a29be24319f23c43fd6361e582785b
PranjaliBhute <bhutepranjali@gmail.com>
2022-09-18 15:23:54 +0530

    Creating customer table

---
+++ SQL/SQL_practice_query/customer.sql
@@ -0,0 +1,5 @@
+select * from product
+create table customer(custid int primary key, custname varchar(20))
+insert into customer values(1, 'Saanvi')
+insert into customer values(2, 'Pranjali')
+select * from customer 
