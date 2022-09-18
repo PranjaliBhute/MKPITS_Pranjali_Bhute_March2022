0d1140c7c40fe997354359f74db7cc5f3c325af4
PranjaliBhute <bhutepranjali@gmail.com>
2022-09-18 22:57:06 +0530

    Assignment Q1_Create table student

---
+++ SQL/SQL_Assignment_Ques/Assignment1_student.sql
@@ -0,0 +1,15 @@
+create table student(rno int, name varchar(20), address varchar(40), mobno int)
+insert into student values(1, 'Saanvi', 'Pratap nagar', 0123456789)
+insert into student values(1, 'Jaanvi', 'Pratap nagar', 1023456789)
+insert into student values(1, 'Pranjali', 'Laxmi nagar', 1203456789)
+select * from student 
+insert into student values(4, 'Vaibhav', 'Pratap nagar', 1230456789)
+insert into student values(5, 'Kundan', 'Ravi nagar', 1234056789)
+insert into student values(6, 'Komal', 'Chatrapati nagar', 1234506789)
+insert into student values(7, 'Ravina', 'Bajaj nagar', 1234560789)
+insert into student values(9, 'Aniket', 'Ravi nagar', 1234567089)
+select * from student 
+insert into student values(10, 'Nilam', 'Pratap nagar', 1234567809)
+select * from student 
+insert into student values(8, 'Vaibhavi', 'Pratap nagar', 1234567890)
+select * from student
