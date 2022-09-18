e506dbeccd0fb0c11cf6072f32379dcc281e8869
PranjaliBhute <bhutepranjali@gmail.com>
2022-09-18 23:07:10 +0530

    Create table users

---
+++ SQL/SQL_Assignment_Ques/Assignment3_users.sql
@@ -0,0 +1,13 @@
+create table users(userid int, username varchar(15), gender char, emailid varchar(40), password varchar(20), city varchar(15), country varchar(15))
+insert into users values(1, 'Pranjali', 'F', 'bhutepranjali@gmail.com', 'bhutepranjali', 'Nagpur', 'India')
+select * from users
+insert into users values(2, 'Vaibhav', 'M', 'bhutevaibhav@gmail.com', 'bhutepranjali', 'Nagpur', 'India')
+insert into users values(3, 'Saanvi', 'F', 'bhutesaanvi@gmail.com', 'bhutesaanvi', 'Nagpur', 'India')
+insert into users values(4, 'Nilam', 'F', 'bhutenilam@gmail.com', 'bhutenilam', 'Mumbai', 'India')
+insert into users values(5, 'Aniket', 'M', 'bhuteaniket@gmail.com', 'bhuteaniket', 'Mumbai', 'India')
+insert into users values(6, 'Kundan', 'M', 'shendekundan@gmail.com', 'shendekundan', 'Pune', 'India')
+insert into users values(7, 'Ravina', 'F', 'shenderavina@gmail.com', 'shenderavina', 'Pune', 'India')
+insert into users values(8, 'Komal', 'F', 'bhutekomal@gmail.com', 'bhutekomal', 'Nagpur', 'India')
+insert into users values(9, 'Ramesh', 'M', 'shenderamesh@gmail.com', 'shenderamesh', 'Warora', 'India')
+insert into users values(10, 'Vijaya', 'F', 'shendevijaya@gmail.com', 'shendevijaya', 'Warora', 'India')
+select * from users
