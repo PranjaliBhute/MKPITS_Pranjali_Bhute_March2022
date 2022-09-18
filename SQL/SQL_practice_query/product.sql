6235051635b4670c1b45db4bf59bbdfe38381e1e
PranjaliBhute <bhutepranjali@gmail.com>
2022-09-18 18:26:19 +0530

    Create table product

---
+++ SQL/SQL_practice_query/product.sql
@@ -0,0 +1,5 @@
+create table product(productid int, productname varchar(20), price int, quantity int)
+insert into product values(1, 'Pen', 10, 2)
+insert into product values(2, 'Boll', 15, 2)
+insert into product values(3, 'Notebook', 100, 4)
+select * from product
