9b79f80cfbcadc3e6fc5123324c58e330190fc0f
PranjaliBhute <bhutepranjali@gmail.com>
2022-09-18 23:05:44 +0530

    Assignment Q2_Create table customer

---
+++ SQL/SQL_Assignment_Ques/Assignment2_customer.sql
@@ -0,0 +1,12 @@
+create table customer(custid int, custname varchar(20), custaddress varchar(40))
+insert into customer values(1, 'Saanvi', 'Pratap nagar')
+insert into customer values(2, 'Pranjali', 'Bajaj nagar')
+insert into customer values(3, 'Komal', 'Ravi nagar')
+insert into customer values(4, 'Pranjal', 'Bajaj nagar')
+insert into customer values(5, 'Vaibhav', 'Laxmi nagar')
+insert into customer values(6, 'Ravina', 'Chatrapati nagar')
+insert into customer values(7, 'Kundan', 'Bajaj nagar')
+insert into customer values(8, 'Aniket', 'Pratap nagar')
+insert into customer values(9, 'Nilam', 'Ravi nagar')
+insert into customer values(10, 'Vaibhav', 'Pratap nagar')
+select * from customer
