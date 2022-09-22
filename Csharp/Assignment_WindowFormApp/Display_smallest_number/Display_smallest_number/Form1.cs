dba563f97d2383a9ffb450e6e1fde05cd9d3b811
PranjaliBhute <bhutepranjali@gmail.com>
2022-09-22 21:01:31 +0530

    Program to display smallest number

---
+++ Csharp/Assignment_WindowFormApp/Display_smallest_number/Display_smallest_number/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Display_smallest_number
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n1 = Convert.ToInt32(textBox1.Text);
+            int n2 = Convert.ToInt32(textBox2.Text);
+            if (n1 < n2)
+            {
+                label3.Text = "n1 is smallest number.";
+            }
+            else
+            {
+                label3.Text = "n2 is smallest number.";
+            }
+        }
+    }
+}
