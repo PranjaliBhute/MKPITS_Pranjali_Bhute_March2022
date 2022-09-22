f11d0a3c498fd04765b443e6de47f41973cc22ed
PranjaliBhute <bhutepranjali@gmail.com>
2022-09-22 18:19:52 +0530

    Program to accept a number and print table of that number

---
+++ Csharp/PracticeProgram_WindowsFormApp/WindowsFormsApp11/WindowsFormsApp11/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            int num = Convert.ToInt32(textBox1.Text);
+            int result = 1;
+            int i = 1;
+            while(i<=10)
+            {
+                result =  num * i;
+                sb.Append(num + " * " + i + " = " + result + "\n");
+                i++;
+            }
+            label2.Text = sb.ToString();
+        }
+    }
+}
