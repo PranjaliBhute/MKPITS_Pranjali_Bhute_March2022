930baec3dfd92071b19551fc87f3ce923ef60aed
PranjaliBhute <bhutepranjali@gmail.com>
2022-09-22 18:55:59 +0530

    Create and using classes example

---
+++ Csharp/PracticeProgram_WindowsFormApp/Create and using classes example/Create and using classes example/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Create_and_using_classes_example
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Student s = new Student();
+            s.rollno = Convert.ToInt32(textBox1.Text);
+            s.name = textBox2.Text;
+            s.email = textBox3.Text;
+            s.mobileno = Convert.ToInt64(textBox4.Text);
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Rollno = " + s.rollno + "\n");
+            sb.Append("Name = " + s.name + "\n");
+            sb.Append("Email = " + s.email + "\n");
+            sb.Append("Mobile no = " + s.mobileno + "\n");
+            label5.Text = sb.ToString();
+        }
+
+    }
+}
+
---
+++ Csharp/PracticeProgram_WindowsFormApp/Create and using classes example/Create and using classes example/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Create_and_using_classes_example
+{
+     class Student
+    {
+        public int rollno;
+        public string name;
+        public string email;
+        public long mobileno;
+
+    }
+}
