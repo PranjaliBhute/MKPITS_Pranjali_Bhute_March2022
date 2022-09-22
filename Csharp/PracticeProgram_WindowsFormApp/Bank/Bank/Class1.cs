ed31a9fe0053d5dd91e323c6b3e011e8fb5ad314
PranjaliBhute <bhutepranjali@gmail.com>
2022-09-22 18:58:35 +0530

    Example of Bank

---
+++ Csharp/PracticeProgram_WindowsFormApp/Bank/Bank/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    public abstract class Account
+    {
+        public int accountno;
+        public int balance = 1000;
+        public abstract string deposit(int amount);
+        public string withdrawl(int amount)
+        {
+            string res = null;
+            if (balance > amount)
+            {
+                res = "Insuffient amount , cannot withdrawl money!";
+            }
+            else
+            {
+                balance = balance - amount;
+                res = "Amount withdrawl successfully , balance is = " + balance.ToString();
+            }
+            return res;
+        }
+        public string showbalance()
+        {
+            return "Balance amount is = " + balance.ToString();
+        }
+    }
+    public class Saving : Account
+    {
+        public override string deposit(int amount)
+        {
+            int interest = 300;
+            balance = balance + amount + interest;
+            return "Amount deposited successfully,balance is = " + balance.ToString();
+        }
+    }
+    public class Current : Account
+    {
+        public override string deposit(int amount)
+        {
+            balance = balance + amount;
+            return "Amount deposite successfully,balance is = " + balance.ToString();
+
+        }
+    }
+}
