d3eed9c4dcceba039773534a4eca5f188bb0a944
PranjaliBhute <bhutepranjali@gmail.com>
2022-09-22 18:53:00 +0530

    Another example with runtime polymorphism

---
+++ Csharp/PracticeProgram_WindowsFormApp/AnitherExWithRuntimePolymorphism/AnitherExWithRuntimePolymorphism/Account.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnitherExWithRuntimePolymorphism
+{
+    abstract class Account
+    {
+        public int accountno;
+        public int balance = 1000;
+        public abstract string deposit(int amount);
+
+        public string withdrawl(int amount)
+        {
+            string res = null;
+            if (amount > balance)
+            {
+                res = "Insufficient amount , cannot withdraw money.";
+            }
+            else
+            {
+                balance = balance - amount;
+                res = "Amount withdrawl successfully, balance is " + balance.ToString();
+            }
+            return res;
+        }
+        public string showbalance()
+        {
+            return "Balance amount is = " + balance.ToString();
+        }
+
+    }
+    class Saving : Account
+    {
+        public override string deposit(int amount)
+        {
+            int interest = 500;
+            balance = balance + amount + interest;
+            return "Amount deposited successfully with interest , balance is = " + balance.ToString();
+
+        }
+    }
+    class Current : Account
+    {
+        public override string deposit(int amount)
+        {
+
+            balance = balance + amount;
+            return "Amount deposited successfully without interest , balance is = " + balance.ToString();
+
+        }
+    }
+}
---
+++ Csharp/PracticeProgram_WindowsFormApp/AnitherExWithRuntimePolymorphism/AnitherExWithRuntimePolymorphism/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnitherExWithRuntimePolymorphism
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Account act = null;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            act.accountno = Convert.ToInt32(textBox1.Text);
+            string str = act.deposit(Convert.ToInt32(textBox2.Text));
+            label4.Text = str;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            act.accountno = Convert.ToInt32(textBox1.Text);
+            string str = act.withdrawl(Convert.ToInt32(textBox2.Text));
+            label4.Text = str;
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            act.accountno = Convert.ToInt32(textBox1.Text);
+            string str = act.showbalance();
+            label4.Text = "Account no = " + act.accountno + " and " + str;
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "Saving")
+            {
+                act = new Saving();
+            }
+            else if (comboBox1.Text == "Current")
+            {
+                act = new Current();
+            }
+        }
+    }       
+
+}
